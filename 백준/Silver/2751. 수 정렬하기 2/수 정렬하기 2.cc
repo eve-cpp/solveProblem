7c389a77c01458081fd155cd052f239db9c66569
eve.cpp <71521487+eve-cpp@users.noreply.github.com>
2023-11-01 21:31:16 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 680 ms, Memory: 5808 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int arr[1000001];
+    int n;
+
+    cin >> n;
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    sort(arr, arr + n);
+
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << '\n';
+}

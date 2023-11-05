396387095d5aee0b5b8ccc0749187eb18b4aa194
eve.cpp <71521487+eve-cpp@users.noreply.github.com>
2023-11-05 22:51:55 +0900

    [Bronze IV] Title: 베라의 패션, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include<algorithm>
+#include <string>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	int N;
+	cin >> N;
+
+	cout << N * (N - 1) << endl;
+
+}
